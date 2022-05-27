4016453865bd2451181315fe52c0d50876a18869
Milan Biranwar <milanbiranwar06@gmail.com>
2022-05-27 16:41:57 +0530

    Add project files.

---
+++ AddressBookDAY9/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddressBookDAY9;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+Console.WriteLine("Hello, World!");
+
+AddressBook addressBook = new AddressBook();
+addressBook.AddNewContact();
+addressBook.AddNewContact();
+
+AddressBook.PrintAddressBook();
+Console.WriteLine("Enter the FirstName to Edit");
+string firstName = Console.ReadLine();
+addressBook.EditContact(firstName);
+AddressBook.PrintAddressBook();
+
+
+AddressBook.PrintAddressBook();
+Console.WriteLine("Enter the FirstName to Delete");
+string firstName1 = Console.ReadLine();
+addressBook.DeleteContact(firstName1);
+AddressBook.PrintAddressBook();
+Console.ReadLine();
